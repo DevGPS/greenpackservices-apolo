290f9809cadb8d08ff3fc8f72d9a8d9960d109de
DevGPS <97750371+DevGPS@users.noreply.github.com>
2022-12-06 14:22:41 -0300

    Revert "todo ok 11:46am 0612"
    This reverts commit 947ea7773c1a1a2517ffcd9fc1cd8210d92f3215.

--- config/settings.py
+++ config/settings.py
@@ -156,23 +156,22 @@ REST_FRAMEWORK = {
 }
 
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 DEBUG = True
-# if not DEBUG:    
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#     STATIC_TMP = os.path.join(BASE_DIR, 'static/')
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_TMP = os.path.join(BASE_DIR, 'static/')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 # STATIC_TMP = os.path.join(BASE_DIR, 'static/')
 
 
-
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 
 
