947ea7773c1a1a2517ffcd9fc1cd8210d92f3215
DevGPS <97750371+DevGPS@users.noreply.github.com>
2022-12-06 13:53:55 -0300

    todo ok 11:46am 0612

--- config/settings.py
+++ config/settings.py
@@ -156,22 +156,23 @@ REST_FRAMEWORK = {
 }
 
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 DEBUG = True
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_TMP = os.path.join(BASE_DIR, 'static/')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# if not DEBUG:    
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#     STATIC_TMP = os.path.join(BASE_DIR, 'static/')
+#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 # STATIC_TMP = os.path.join(BASE_DIR, 'static/')
 
 
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 
