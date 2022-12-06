1a4e1093543e32c45a1ed9a667a6c4961296a81c
DevGPS <97750371+DevGPS@users.noreply.github.com>
2022-12-06 10:58:28 -0300

    todo ok 10:46am 0612

--- config/settings.py
+++ config/settings.py
@@ -183,7 +183,7 @@ if not DEBUG:
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 
 # STATIC_TMP = os.path.join(BASE_DIR, 'static/')
