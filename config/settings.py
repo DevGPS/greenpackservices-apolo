218b65d1d9b63dae3f77e94cbbd9e57e02fdac26
DevGPS <97750371+DevGPS@users.noreply.github.com>
2022-12-06 11:26:28 -0300

    todo ok 10:46am 0612

--- config/settings.py
+++ config/settings.py
@@ -164,6 +164,7 @@ if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
