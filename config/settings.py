f1904340135d0988b68e19c72b770a8b96d64086
DevGPS <97750371+DevGPS@users.noreply.github.com>
2022-12-06 10:49:51 -0300

    todo ok 10:46am 0612

--- config/settings.py
+++ config/settings.py
@@ -175,13 +175,13 @@ REST_FRAMEWORK = {
 
 STATIC_URL = '/static/'
 DEBUG = True
-# if not DEBUG:    
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#     STATIC_TMP = os.path.join(BASE_DIR, 'static/')
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+if not DEBUG:    
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_TMP = os.path.join(BASE_DIR, 'static/')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 # STATIC_TMP = os.path.join(BASE_DIR, 'static/')
